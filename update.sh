35b6d05ce3a5979b01305771dfecc408839e7353
Marko Ivankovic <marko@ivankovic.me>
2017-04-02 14:27:49 +0000

    Fix the update script.

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-wget -O - https://github.com/ivankovic/homepage/archive/master.zip > master.zip
-unzip master.zip
-rsync -ar homepage-master/public/* /var/www/html/
-rm -rf homepage-master
-rm master.zip
+git pull
+rsync -ar public/* /var/www/html/
